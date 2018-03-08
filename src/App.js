256f4823254ab45e08f540d92d42f8572052365f
milengardev <milenford@gmail.com>
2018-03-08 02:15:13 +0100

    Exercise 2 - Without unit testing
    User case(in steps) :
    —Main flow
    1. The user enters the website.
    2. The user clicks on the form.
    3. The form adds clear button.
    3. The user enters text into the input bar.
    
    —Alternative flow 1
    3.1 The user clicks on clear icon
    3.2 The form clears the input
    
    —Alternative flow 2
    3.1 The user clicks outside the form.
    3.2 The form loses focus(blur)
    3.3 The clear icon is hidden

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import Form from './components/Form';
 
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            condition: true,
+            inputValue: ""
+        };
+    }
+
+    onFocusForm(state) {
+        this.setState({
+            condition: state
+        })
+    }
+
+    onBlueForm(state) {
+        this.setState({
+            condition: state
+        })
+    }
+
+    onClickClear(state) {
+        this.setState({
+            inputValue: ""
+        })
+    }
+
+    onOnInputChange(state) {
+        this.setState({
+            inputValue: state
+        })
     }
 
     render() {
         return (
             <div className="container">
-                <Form/>
+                < Form onFocusForm={this.onFocusForm.bind(this)} onBlueForm={this.onBlueForm.bind(this)}
+                       onInputChange={this.onOnInputChange.bind(this)} onClickClear={this.onClickClear.bind(this)}/>
             </div>
         );
     }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,60 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            condition: false,
+            inputValue: ""
+        }
+    }
+
+    handleOnFocus(event) {
+        this.setState({
+            condition: true
+        }, function () {
+            this.props.onFocusForm(this.state.condition);
+        })
+    }
+
+    handleOnClickClearIcon(event) {
+        this.setState({
+            inputValue: ""
+        }, function () {
+            this.props.onClickClear(this.state.condition);
+        })
+
+    }
+    handleOnBlur(event) {
+        setTimeout(() => this.setState({
+            condition: false
+        }, function () {
+            this.props.onFocusForm(this.state.condition);
+        }), 250)
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        }, function () {
+            this.props.onInputChange(this.state.condition);
+        })
+    }
+
     render() {
         return (
             <div className="box">
-                <form>
-                    <input type="text" id="zoeken" name="" placeholder="Zoeken"/>
-                    <button className="icon"><span>icon</span></button>
+                <form onFocus={this.handleOnFocus.bind(this)} onBlur={this.handleOnBlur.bind(this)}>
+                    <input type="text" id="zoeken" name="" value={this.state.inputValue} placeholder="Zoeken"
+                           ref="inputValue" onChange={this.handleInputChange.bind(this)}/>
+                    <button type="button" onClick={this.handleOnClickClearIcon.bind(this)}
+                            className={this.state.condition ? "icon icon-clear" : "icon icon-clear visibility-hide"}>
+                        <span>X</span>
+                    </button>
+                    <button className="icon icon-search">
+                        <span>icon</span>
+                    </button>
                 </form>
             </div>
         );
